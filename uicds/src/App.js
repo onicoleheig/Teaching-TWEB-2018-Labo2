2ab09aca61c0b3a1450fe5d100550fe5baca827c
Olivier Nicole <olivierncl@gmail.com>
2018-11-21 21:41:40 +0100

    try - doesn't work yet

--- uicds/src/App.js
+++ uicds/src/App.js
@@ -1,29 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom'
+import gql from 'graphql-tag'
+
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+import { AUTH_TOKEN } from './constants'
+
+import IssueTable from './components/IssuesTable';
+
+const httpLink = createHttpLink({
+  uri: 'https://api.github.com/graphql',
+  headers: {
+    authorization: `Bearer ${AUTH_TOKEN}`,
+  }
+})
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
+let itemsList = [
+  {status:"opened", title:"The keyboard shows and hide when clicking on input in android/chrome", activity:"#14063 opened 36 minutes ago by Aykelith", nbComments:"10"},
+  {status:"closed", title:"The keyboard shows and hide when clicking on input in android/chrome", activity:"#14063 opened 36 minutes ago by Aykelith", nbComments:"44"},
+  {status:"opened", title:"The keyboard shows and hide when clicking on input in android/chrome", activity:"#14063 opened 36 minutes ago by Aykelith", nbComments:"45"},
+  {status:"closed", title:"The keyboard shows and hide when clicking on input in android/chrome", activity:"#14063 opened 36 minutes ago by Aykelith", nbComments:"0"},
+  {status:"closed", title:"The keyboard shows and hide when clicking on input in android/chrome", activity:"#14063 opened 36 minutes ago by Aykelith", nbComments:"62"}
+];
+
+const graphQLResquest = gql`
+  query {
+    repository(owner: "facebook", name: "react") {
+      issues(first: 10) {
+        edges {
+          cursor
+          node {
+            title
+            state
+            author { login }
+            body
+            createdAt
+            comments { totalCount }
+          }
+        }
+      }
+    }
+  }
+`
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <IssueTable items={itemsList}></IssueTable>
       </div>
     );
   }
 }
+ReactDOM.render( <ApolloProvider client={client}>
+  
+  <App />
 
+</ApolloProvider>, 
+document.getElementById('root'));
 
 export default App;
